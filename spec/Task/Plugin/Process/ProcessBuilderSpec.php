5bdb4366592510f05a247e5154357027335e884e
Mike Fisher <mike@mbfisher.com>
2014-05-03 10:20:49 +0100

    Update specs

--- spec/Task/Plugin/Process/ProcessBuilderSpec.php
+++ spec/Task/Plugin/Process/ProcessBuilderSpec.php
@@ -5,10 +5,41 @@ namespace spec\Task\Plugin\Process;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+use Task\Plugin\Stream\WritableInterface;
+
 class ProcessBuilderSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('Task\Plugin\Process\ProcessBuilder');
+    }
+
+    function it_should_extend_process()
+    {
+        $this->add('foo')->getProcess()->shouldReturnAnInstanceOf('Task\Plugin\Process\Process');
+    }
+
+    function it_should_run_a_process()
+    {
+        $ps = $this->add('echo')->run();
+        $ps->shouldHaveType('Task\Plugin\Process\Process');
+        $ps->isStarted()->shouldReturn(true);
+    }
+
+    function it_should_read_output()
+    {
+        $this->add('echo')->add('foo')->read()->shouldReturn("foo\n");
+    }
+
+    function it_should_write_to_stdin()
+    {
+        $ps = $this->add('cat')->write('foo');
+        $ps->getStdin()->shouldReturn('foo');
+        $ps->isStarted()->shouldReturn(true);
+    }
+
+    function it_should_pipe_to_process(WritableInterface $to)
+    {
+        $this->add('echo')->pipe($to);
     }
 }
--- spec/Task/Plugin/ProcessPluginSpec.php
+++ spec/Task/Plugin/ProcessPluginSpec.php
@@ -11,4 +11,39 @@ class ProcessPluginSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Task\Plugin\ProcessPlugin');
     }
+
+    function it_should_init_a_process_builder()
+    {
+        $ps = $this->build('foo', ['bar']);
+        $ps->shouldHaveType('Task\Plugin\Process\ProcessBuilder');
+        $ps->getProcess()->getCommandLine()->shouldReturn("'foo' 'bar'");
+    }
+
+    function it_should_run_a_process()
+    {
+        $tmp = sys_get_temp_dir();
+        $env = ['FOO' => 'BAR'];
+        $ps = $this->run('echo', ['foo'], $tmp, $env);
+
+        $ps->shouldHaveType('Task\Plugin\Process\Process');
+        $ps->isStarted()->shouldReturn(true);
+        $ps->getCommandLine()->shouldReturn("'echo' 'foo'");
+        $ps->getWorkingDirectory()->shouldReturn($tmp);
+        $ps->getEnv()->shouldContainArray($env);
+    }
+    
+    public function getMatchers()
+    {
+        return [
+            'containArray' => function ($subject, $data) {
+                $pass = true;
+
+                foreach ($data as $key => $value) {
+                    $pass = $pass && array_key_exists($key, $subject) && $subject[$key] == $value;
+                }
+
+                return $pass;
+            }
+        ];
+    }
 }
